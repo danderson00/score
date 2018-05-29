b314ff4d0203dbb1dd2ce08827219dfb8bfda1de
danderson00 <danderson00@gmail.com>
2018-05-29 15:15:52 +1000

    Add readme detail and comments

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a sample application for the tribe platform.
 
-## Play
+## Install and Run
 
 ```
 git clone https://github.com/danderson00/score.git
@@ -15,3 +15,16 @@ Then open two browser tabs to http://localhost:1678/.
 
 The application is intended to run on a mobile device. If you're using Chrome, enable device mode
 in the developer tools. Even better, open port 1678 in your firewall and hit it from your mobile phone.
+
+## Overview
+
+There are four main folders in the scoring application:
+
+---|---
+vocabulary | Rx expressions that can be reused in other expressions throughout the app
+panes      | User interface components for the application
+facets     | Facets allow stitching together multiple expressions and events into discrete components
+flows      | Flows define the navigation flow for the app
+
+For a more detailed overview of the concepts behind the application, check 
+[this post]().
--- facets/game.js
+++ facets/game.js
@@ -1,6 +1,7 @@
 require('tribe').register.facet(function (facet) {
     facet.isDistributed()
-    
+
+    // make certain pieces of state available through observables
     this.lead = facet.envelopes.lead().asScalar()
     this.playerIds = facet.envelopes.allPlayers().select(x => x.key).asArray()
     this.winner = ko.observable()
@@ -10,9 +11,11 @@ require('tribe').register.facet(function (facet) {
         .forEach(player => player.points()
             .when(points => points >= 3 && this.lead() >= 2)
             .then(() => this.winner(player.key)))
-        
+    
+    // publish the game.over event when the winner observable is set
     this.winner.subscribe(winner => facet.publish('game.over', { playerId: this.playerIds(), winner: winner }))
 
+    // this handles scoring a game by specifying the outcome, i.e. number of points per player, in a single message
     facet.handles('points', (data) => {
         data.forEach(x => facet.publish({ topic: 'point', data: x, silent: true }))
         if(this.winner())
